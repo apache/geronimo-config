3389a74c453136f267a9c1c7089c7cf3f54ee8ce
Mark Struberg <struberg@apache.org>
2017-08-07 21:42:39 +0000

    GERONIMO-6577 add test to show what's broken
    got broken with r1800744.
    I'm also not a big fan of this change as it introduced a lot complexity without much benefit.
    It actually destroyed the ability to do programmatic lookup.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/components/config/trunk@1804361 13f79535-47bb-0310-9956-ffa450edef68

---
+++ impl/src/test/java/org/apache/geronimo/config/test/internal/ProviderTest.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.geronimo.config.test.internal;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Provider;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class ProviderTest extends Arquillian {
+    private static final String SOME_KEY = "org.apache.geronimo.config.test.internal.somekey";
+
+    @Deployment
+    public static WebArchive deploy() {
+        JavaArchive testJar = ShrinkWrap
+                .create(JavaArchive.class, "configProviderTest.jar")
+                .addClasses(ProviderTest.class, SomeBean.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .as(JavaArchive.class);
+
+        WebArchive war = ShrinkWrap
+                .create(WebArchive.class, "providerTest.war")
+                .addAsLibrary(testJar);
+        return war;
+    }
+
+    private @Inject SomeBean someBean;
+
+
+    @Test
+    public void testConfigProvider() {
+        System.setProperty(SOME_KEY, "someval");
+        String myconfig = someBean.getMyconfig();
+        Assert.assertEquals(myconfig, "someval");
+
+        System.setProperty(SOME_KEY, "otherval");
+        myconfig = someBean.getMyconfig();
+        Assert.assertEquals(myconfig, "otherval");
+    }
+
+
+    @RequestScoped
+    public static class SomeBean {
+
+        @Inject
+        @ConfigProperty(name=SOME_KEY)
+        private Provider<String> myconfig;
+
+        public String getMyconfig() {
+            return myconfig.get();
+        }
+    }
+}
