d133f11f67a19f5943fd80a7ca3649cc66f6584b
Mark Struberg <struberg@apache.org>
2018-06-08 09:12:42 +0000

    fix implicit converter rules and upgrade to mp-config-1.3
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/components/config/trunk@1833154 13f79535-47bb-0310-9956-ffa450edef68

--- impl/src/main/java/org/apache/geronimo/config/converters/ImplicitConverter.java
+++ impl/src/main/java/org/apache/geronimo/config/converters/ImplicitConverter.java
@@ -35,15 +35,24 @@ public abstract class ImplicitConverter {
 
     public static Converter getImplicitConverter(Class<?> clazz) {
         // handle ct with String param
-        Converter converter = hasConverterCt(clazz, String.class);
+        Converter converter = null;
         if (converter == null) {
-            converter = hasConverterCt(clazz, CharSequence.class);
+            converter = hasConverterMethod(clazz, "of", String.class);
+        }
+        if (converter == null) {
+            converter = hasConverterMethod(clazz, "of", CharSequence.class);
         }
         if (converter == null) {
             converter = hasConverterMethod(clazz, "valueOf", String.class);
         }
         if (converter == null) {
             converter = hasConverterMethod(clazz, "valueOf", CharSequence.class);
+        }
+        if (converter == null) {
+            converter = hasConverterCt(clazz, String.class);
+        }
+        if (converter == null) {
+            converter = hasConverterCt(clazz, CharSequence.class);
         }
         if (converter == null) {
             converter = hasConverterMethod(clazz, "parse", String.class);
@@ -66,7 +75,7 @@ public abstract class ImplicitConverter {
                     try {
                         return declaredConstructor.newInstance(value);
                     } catch (Exception e) {
-                        throw new RuntimeException(e);
+                        throw new IllegalArgumentException(e);
                     }
                 }
             };
